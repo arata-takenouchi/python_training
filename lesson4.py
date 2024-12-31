b1e1d081e7469d8d86456297febf9938624e27f8
arata-takenouchi <arata.takenouchi.1230@gmail.com>
2024-12-31 19:54:35 +0900

    lesson4-2

--- lesson4.py
+++ lesson4.py
@@ -15,3 +15,18 @@ print('Py''thon')
 s = ('aaaaaaaaaaaaaaaaaa'
      'bbbbbbbbbbbbbbbbbb')
 print(s)
+
+word = 'python'
+# print(word[0])
+# print(word[1])
+print(word[-1])
+print(word[0:2])
+print(word[2:5])
+print(word[:6])
+print(word[2:])
+
+word = 'j' + word[1:]
+print(word[:])
+
+n = len(word)
+print(n)
