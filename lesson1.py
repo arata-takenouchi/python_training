430dcb116ab006f1ca711fc99ed17c00d9299610
arata-takenouchi <arata.takenouchi.1230@gmail.com>
2024-12-31 19:15:27 +0900

    lesson1

---
+++ lesson1.py
@@ -0,0 +1,19 @@
+# num = 1
+# name = 'Mike'
+# bool = True
+# name = num
+# print(num, type(num))
+# print(name, type(name))
+# print(bool, type(bool))
+
+# 1num: int = 1
+num: int = 1
+name: str = '1'
+
+# if = 'value'
+
+# new_num = int(name)
+# print(new_num, type(new_num))
+
+num = name
+print(num, type(num))
