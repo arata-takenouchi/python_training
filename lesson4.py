f2755103d84def608d8d7721e316cb867800736f
arata-takenouchi <arata.takenouchi.1230@gmail.com>
2024-12-31 19:48:42 +0900

    lesson4

---
+++ lesson4.py
@@ -0,0 +1,17 @@
+print('I don\'t know')
+print('say "I don\'t know"')
+print("say \"I don't know\"")
+print(r'C:\name\name')
+
+print("""\
+line1
+line2
+line3\
+""")
+
+print('Hi.' * 3 + 'Mike.')
+print('Py''thon')
+
+s = ('aaaaaaaaaaaaaaaaaa'
+     'bbbbbbbbbbbbbbbbbb')
+print(s)
