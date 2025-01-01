d00f3661f3ba474adb675060cc9a03b71d48492b
arata-takenouchi <arata.takenouchi.1230@gmail.com>
2025-01-01 22:54:54 +0900

    lesson12

---
+++ lesson12.py
@@ -0,0 +1,34 @@
+y = [1,2,3]
+x = 1
+
+if x in y:
+     print('in')
+
+if 100 not in y:
+     print('not in')
+
+# a = 1
+# b = 2
+# if a != b:
+#      print('Not equal')
+# if a != b:
+#      print('Not equal')
+
+# is_ok = True
+# if is_ok:
+#      print('hello')
+
+# is_ok = [1,2,3,4]
+# if is_ok:
+#      print('OK')
+# else:
+#      print('No')
+
+is_empty = None
+# if is_empty == None:
+if is_empty is not None:
+     print('None')
+
+print(1 == True)
+print(1 is True)
+print(True is True)
