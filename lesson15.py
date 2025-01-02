16cfb604f80f8d4b1118fd45dcb0c2a8946c130c
arata-takenouchi <arata.takenouchi.1230@gmail.com>
2025-01-02 23:07:06 +0900

    lesson15-2

--- lesson15.py
+++ lesson15.py
@@ -1,4 +1,4 @@
-some_list = [1,2,3,4,5]
+# some_list = [1,2,3,4,5]
 
 # i = 0
 # while i < len(some_list):
@@ -11,7 +11,15 @@ some_list = [1,2,3,4,5]
 # for s in 'abcde':
 #      print(s)
 
-for word in ['My', 'name', 'is', 'Mike']:
-     if word == 'name':
-          continue
-     print(word)
+# for word in ['My', 'name', 'is', 'Mike']:
+#      if word == 'name':
+#           continue
+#      print(word)
+
+for fruit in ['apple', 'banana', 'orange']:
+    if fruit == 'banana':
+        print('stop')
+        break
+    print(fruit)
+else:
+    print('all')
