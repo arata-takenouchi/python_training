cee6a3951d256673ee15d06fe14af9d44da678a5
arata-takenouchi <arata.takenouchi.1230@gmail.com>
2025-01-03 22:33:43 +0900

    lesson22-2

--- lesson22.py
+++ lesson22.py
@@ -1,15 +1,35 @@
-"""
-Test #########
-"""
-animal = 'cat'
+# """
+# Test #########
+# """
+# animal = 'cat'
 
-def f():
-    """Test func doc"""
-    print(f.__name__)
-    print(f.__doc__)
+# def f():
+#     """Test func doc"""
+#     print(f.__name__)
+#     print(f.__doc__)
     # global animal
     # animal = 'dog'
     # print('local', locals())
-f()
+# f()
 # print('global:', globals())
-print(__name__)
+# print(__name__)
+
+
+l = [1,2,3]
+i = 5
+# del l
+
+try:
+    # () + l
+    l[0]
+    # l[i]
+except IndexError as e:
+    print(f'Index Error: {e}')
+except NameError as e:
+    print(f'Name Error: {e}')
+except Exception as e:
+    print(f'Other Error: {e}')
+else:
+    print('done')
+finally:
+    print('clean up')
