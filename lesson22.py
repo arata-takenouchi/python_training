c68e19cdb15c97a64acbd68ddf15ac1a63af68b1
arata-takenouchi <arata.takenouchi.1230@gmail.com>
2025-01-03 22:41:47 +0900

    lesson22-3

--- lesson22.py
+++ lesson22.py
@@ -15,21 +15,36 @@
 # print(__name__)
 
 
-l = [1,2,3]
-i = 5
+# l = [1,2,3]
+# i = 5
 # del l
 
-try:
+# try:
     # () + l
-    l[0]
+    # l[0]
     # l[i]
-except IndexError as e:
-    print(f'Index Error: {e}')
-except NameError as e:
-    print(f'Name Error: {e}')
-except Exception as e:
-    print(f'Other Error: {e}')
-else:
-    print('done')
-finally:
-    print('clean up')
+# except IndexError as e:
+#     print(f'Index Error: {e}')
+# except NameError as e:
+#     print(f'Name Error: {e}')
+# except Exception as e:
+#     print(f'Other Error: {e}')
+# else:
+#     print('done')
+# finally:
+#     print('clean up')
+
+
+# raise IndexError('test error')
+class UppercaseError(Exception):
+    pass
+
+def check():
+    words = ['APPLE', 'orange', 'banana']
+    for word in words:
+        if word.isupper():
+            raise UppercaseError(word)
+try:
+    check()
+except UppercaseError as e:
+    print('This is my fault. Go next')
