57172803861c7eeadedd65016984bdddb1e26b9f
arata-takenouchi <arata.takenouchi.1230@gmail.com>
2025-01-03 22:09:17 +0900

    lesson21-3

--- lesson21.py
+++ lesson21.py
@@ -23,13 +23,23 @@
 # print(r)
 
 
-w = ['mon', 'tue', 'wed']
-f = ['coffee', 'milk', 'water']
+# w = ['mon', 'tue', 'wed']
+# f = ['coffee', 'milk', 'water']
+#
+# d = {}
+# for x, y in zip(w, f):
+#     d[x] = y
+# print(d)
+#
+# d = {x: y for x, y in zip(w, f)}
+# print(d)
 
-d = {}
-for x, y in zip(w, f):
-    d[x] = y
-print(d)
 
-d = {x: y for x, y in zip(w, f)}
-print(d)
+s = set()
+for i in range(10):
+    if i % 2 == 0:
+        s.add(i)
+print(s)
+
+s = {i for i in range(10) if i % 2 == 0}
+print(s)
