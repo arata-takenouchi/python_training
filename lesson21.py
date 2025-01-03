bc93c20e7bcaebb4bd2f32d87852e70236b4e4bc
arata-takenouchi <arata.takenouchi.1230@gmail.com>
2025-01-03 21:35:21 +0900

    lesson21-2

--- lesson21.py
+++ lesson21.py
@@ -1,23 +1,35 @@
-t = (1,2,3,4,5)
-t2 = (5,6,7,8,9,10)
+# t = (1,2,3,4,5)
+# t2 = (5,6,7,8,9,10)
+#
+# r = []
+# for i in t:
+#     if i % 2 == 0:
+#         r.append(i)
+#
+# print(r)
+#
+# r = [i for i in t if i % 2 == 0]
+# print(r)
+#
+# r = []
+# for i in t:
+#     for j in t2:
+#         r.append(i * j)
+#
+# print(r)
+#
+# r = [i * j for i in t for j in t2]
+#
+# print(r)
 
-r = []
-for i in t:
-    if i % 2 == 0:
-        r.append(i)
 
-print(r)
+w = ['mon', 'tue', 'wed']
+f = ['coffee', 'milk', 'water']
 
-r = [i for i in t if i % 2 == 0]
-print(r)
+d = {}
+for x, y in zip(w, f):
+    d[x] = y
+print(d)
 
-r = []
-for i in t:
-    for j in t2:
-        r.append(i * j)
-
-print(r)
-
-r = [i * j for i in t for j in t2]
-
-print(r)
+d = {x: y for x, y in zip(w, f)}
+print(d)
