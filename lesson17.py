ad9e08e32c6decf11cd7ae7a4aa6d499a55b3ca1
arata-takenouchi <arata.takenouchi.1230@gmail.com>
2025-01-03 19:24:56 +0900

    lesson17

--- lesson17.py
+++ lesson17.py
@@ -1,20 +1,26 @@
-def say_something():
-    # print('hi')
-    s = 'hi'
-    return s
-# say_something()
-result = say_something()
-print(result)
+# def say_something():
+#     s = 'hi'
+#     return s
+# result = say_something()
+# print(result)
+#
+# def what_is_this(color):
+#     if color == 'red':
+#         return 'tomato'
+#     elif color == 'green':
+#         return 'green pepper'
+#     else:
+#         return "I don't know"
+#
+# result = what_is_this('green')
+# result = what_is_this('red')
+# result = what_is_this('yellow')
+# print(result)
 
-def what_is_this(color):
-    if color == 'red':
-        return 'tomato'
-    elif color == 'green':
-        return 'green pepper'
-    else:
-        return "I don't know"
 
-result = what_is_this('green')
-result = what_is_this('red')
-result = what_is_this('yellow')
-print(result)
+# num: int = 10
+def add_num(a: int, b: int) -> int:
+    return a + b
+
+r = add_num(10, 20)
+print(r)
