023e107d87e5d868ed1e50b85729819b4357b81c
arata-takenouchi <arata.takenouchi.1230@gmail.com>
2025-01-03 19:39:19 +0900

    lesson17-3

--- lesson17.py
+++ lesson17.py
@@ -26,10 +26,30 @@
 # print(r)
 
 
-def menu(entree='beef', drink='wine', dessert='ice'):
-    print('entree=', entree)
-    print('drink=', drink)
-    print('dessert=', dessert)
+# def menu(entree='beef', drink='wine', dessert='ice'):
+#     print('entree=', entree)
+#     print('drink=', drink)
+#     print('dessert=', dessert)
+#
+# menu(entree='chicken')
 
-# menu(entree='beef', drink='beer', dessert='ice')
-menu(entree='chicken')
+
+def test_func(x, l=None):
+    if l is None:
+        l = []
+    l.append(x)
+    return l
+
+# y = [1,2,3]
+# r = test_func(100, y)
+# print(r)
+#
+# y = [1,2,3]
+# r = test_func(200, y)
+# print(r)
+
+r = test_func(100)
+print(r)
+
+r = test_func(100)
+print(r)
