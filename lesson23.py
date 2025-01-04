611b2af9d9ea1cc9c77935d2e3b1773e86b96877
arata-takenouchi <arata.takenouchi.1230@gmail.com>
2025-01-04 18:33:52 +0900

    lesson23-5

--- lesson23.py
+++ lesson23.py
@@ -1,22 +1,10 @@
-# import lesson_package.utils
-# from lesson_package.utils import say_twice
+import builtins
+builtins.print()
 
-# from lesson_package.talk import human
-# from lesson_package.talk import animal
-# from lesson_package.talk import *
-#
-# print(animal.sing())
-# print(animal.cry())
+ranking = {
+    'A': 100,
+    'B': 85,
+    'C': 95
+}
 
-# r = utils.say_twice('hello')
-# print(r)
-
-# print(human.sing())
-# print(human.cry())
-
-try:
-    from lesson_package import utils
-except ImportError:
-    from lesson_package.tools import utils
-
-utils.say_twice('word')
+print(sorted(ranking, key=ranking.get, reverse=True))
