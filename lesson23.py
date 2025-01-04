03bd80a940bd738ebe27659637e7044c8f14b06b
arata-takenouchi <arata.takenouchi.1230@gmail.com>
2025-01-04 17:31:17 +0900

    lesson23-3

--- lesson23.py
+++ lesson23.py
@@ -1,11 +1,15 @@
 # import lesson_package.utils
 # from lesson_package.utils import say_twice
 
-from lesson_package.talk import human
+# from lesson_package.talk import human
+# from lesson_package.talk import animal
+from lesson_package.talk import *
 
+print(animal.sing())
+print(animal.cry())
 
 # r = utils.say_twice('hello')
 # print(r)
 
 print(human.sing())
-print(human.cry())
+# print(human.cry())
