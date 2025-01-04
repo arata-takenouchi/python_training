ccbc20bcfb9abc469e9de9c62b436efd5ca3c4c3
arata-takenouchi <arata.takenouchi.1230@gmail.com>
2025-01-04 18:47:00 +0900

    lesson23-7

--- lesson23.py
+++ lesson23.py
@@ -1,20 +1,6 @@
-s = "random aiuaodbhss"
+from termcolor import colored
 
-d = {}
-for c in s:
-    if c not in d:
-        d[c] = 0
-    d[c] += 1
+print('test')
 
-d = {}
-for c in s:
-    d.setdefault(c, 0)
-    d[c] += 1
-print(d)
+print(colored('test', 'green'))
 
-from collections import defaultdict
-
-d = defaultdict(int)
-for c in s:
-    d[c] += 1
-print(d)
