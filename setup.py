212a460f62eb41b86bfda5be120f87787cfa5327
arata-takenouchi <arata.takenouchi.1230@gmail.com>
2025-01-04 17:45:26 +0900

    lesson23-4

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='python_training',
+    version='1.0.0',
+    packages=['lesson_package', 'lesson_package.talk', 'lesson_package.tools'],
+    url='https://about.google/intl/ALL_jp/',
+    license='Free',
+    author='aratatakenouchi',
+    author_email='',
+    description='Sample package'
+)
