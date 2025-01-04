2aa4796697185040e51ebd5d673dda75ee943810
arata-takenouchi <arata.takenouchi.1230@gmail.com>
2025-01-04 19:00:57 +0900

    lesson23-8

--- lesson23.py
+++ lesson23.py
@@ -1,6 +1,18 @@
-from termcolor import colored
+import collections
+import os
+import sys
 
-print('test')
+import termcolor
 
-print(colored('test', 'green'))
+import lesson_package
 
+import config
+
+
+print(collections.__file__)
+print(termcolor.__file__)
+print(lesson_package.__file__)
+print(config.__file__)
+
+
+print(sys.path)
