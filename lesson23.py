05c2394ef19c862dcb5a41788e5129511d0df28e
arata-takenouchi <arata.takenouchi.1230@gmail.com>
2025-01-04 18:39:23 +0900

    lesson23-6

--- lesson23.py
+++ lesson23.py
@@ -1,10 +1,20 @@
-import builtins
-builtins.print()
+s = "random aiuaodbhss"
 
-ranking = {
-    'A': 100,
-    'B': 85,
-    'C': 95
-}
+d = {}
+for c in s:
+    if c not in d:
+        d[c] = 0
+    d[c] += 1
 
-print(sorted(ranking, key=ranking.get, reverse=True))
+d = {}
+for c in s:
+    d.setdefault(c, 0)
+    d[c] += 1
+print(d)
+
+from collections import defaultdict
+
+d = defaultdict(int)
+for c in s:
+    d[c] += 1
+print(d)
