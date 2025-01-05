625ea82e790ed5a40e8e005dbfe2b3ad962186fe
arata-takenouchi <arata.takenouchi.1230@gmail.com>
2025-01-05 22:04:46 +0900

    lesson24-9

--- lesson24.py
+++ lesson24.py
@@ -1,11 +1,12 @@
-class Person(object):
+import abc
+
+class Person(metaclass=abc.ABCMeta):
     def __init__(self, age=1):
         self.age = age
+
+    @abc.abstractmethod
     def drive(self):
-        if self.age >= 18:
-            print('ok')
-        else:
-            raise Exception('No drive')
+        pass
 
 class Baby(Person):
     def __init__(self, age=1):
@@ -13,6 +14,8 @@ class Baby(Person):
             super().__init__(age)
         else:
             raise ValueError
+    def drive(self):
+        raise Exception('No drive')
 
 class Adult(Person):
     def __init__(self, age=18):
@@ -20,6 +23,8 @@ class Adult(Person):
             super().__init__(age)
         else:
             raise ValueError
+    def drive(self):
+        print('ok')
 
 baby = Baby()
 adult = Adult()
