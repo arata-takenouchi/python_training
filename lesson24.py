0900bb4fd3a671d634e9a938a68798965d9cb36e
arata-takenouchi <arata.takenouchi.1230@gmail.com>
2025-01-05 21:48:12 +0900

    lesson24-8

--- lesson24.py
+++ lesson24.py
@@ -1,8 +1,39 @@
+class Person(object):
+    def __init__(self, age=1):
+        self.age = age
+    def drive(self):
+        if self.age >= 18:
+            print('ok')
+        else:
+            raise Exception('No drive')
+
+class Baby(Person):
+    def __init__(self, age=1):
+        if age < 18:
+            super().__init__(age)
+        else:
+            raise ValueError
+
+class Adult(Person):
+    def __init__(self, age=18):
+        if age >= 18:
+            super().__init__(age)
+        else:
+            raise ValueError
+
+baby = Baby()
+adult = Adult()
+
 class Car(object):
     def __init__(self, model=None):
         self.model = model
     def run(self):
         print('run')
+    def ride(self, person):
+        person.drive()
+
+car = Car()
+car.ride(adult)
 
 class ToyotaCar(Car):
     def run(self):
@@ -32,14 +63,14 @@ class TeslaCar(Car):
     def auto_run(self):
         print('auto run')
 
-tesla_car = TeslaCar('Model S', passwd='123')
+# tesla_car = TeslaCar('Model S', passwd='123')
 # tesla_car.__enable_auto_run = True
-print(tesla_car.__enable_auto_run)
+# print(tesla_car.__enable_auto_run)
 
 class T(object):
     pass
 
-t = T()
-t.name = 'Mike'
-t.age = 20
-print(t.name)
+# t = T()
+# t.name = 'Mike'
+# t.age = 20
+# print(t.name)
