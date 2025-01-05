a0684218c07365bb0bba03714113385be53c758d
arata-takenouchi <arata.takenouchi.1230@gmail.com>
2025-01-05 22:12:28 +0900

    lesson24-10

--- lesson24.py
+++ lesson24.py
@@ -1,81 +1,19 @@
-import abc
+class Person(object):
+    def talk(self):
+        print('talk')
 
-class Person(metaclass=abc.ABCMeta):
-    def __init__(self, age=1):
-        self.age = age
-
-    @abc.abstractmethod
-    def drive(self):
-        pass
-
-class Baby(Person):
-    def __init__(self, age=1):
-        if age < 18:
-            super().__init__(age)
-        else:
-            raise ValueError
-    def drive(self):
-        raise Exception('No drive')
-
-class Adult(Person):
-    def __init__(self, age=18):
-        if age >= 18:
-            super().__init__(age)
-        else:
-            raise ValueError
-    def drive(self):
-        print('ok')
-
-baby = Baby()
-adult = Adult()
+    def run(self):
+        print('person run')
 
 class Car(object):
-    def __init__(self, model=None):
-        self.model = model
     def run(self):
-        print('run')
-    def ride(self, person):
-        person.drive()
+        print('car run')
 
-car = Car()
-car.ride(adult)
+class PersonCarRobot(Car, Person):
+    def fly(self):
+        print('fly')
 
-class ToyotaCar(Car):
-    def run(self):
-        print('fast')
-
-class TeslaCar(Car):
-    def __init__(self, model='Model S', enable_auto_run=False, passwd='123'):
-        # self.model = model
-        super().__init__(model)
-        self.__enable_auto_run = enable_auto_run
-        self.passwd = passwd
-
-    @property
-    def enable_auto_run(self):
-        return self._enable_auto_run
-
-    @enable_auto_run.setter
-    def enable_auto_run(self, is_enable):
-        if self.passwd == '456':
-            self._enable_auto_run = is_enable
-        else:
-            raise ValueError
-
-    def run(self):
-        print(self.__enable_auto_run)
-        # print('super fast')
-    def auto_run(self):
-        print('auto run')
-
-# tesla_car = TeslaCar('Model S', passwd='123')
-# tesla_car.__enable_auto_run = True
-# print(tesla_car.__enable_auto_run)
-
-class T(object):
-    pass
-
-# t = T()
-# t.name = 'Mike'
-# t.age = 20
-# print(t.name)
+person_car_robot = PersonCarRobot()
+person_car_robot.talk()
+person_car_robot.run()
+person_car_robot.fly()
