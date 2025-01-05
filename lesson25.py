ca9fd8f1f71cd98b87d38545f0c608cb293d77de
arata-takenouchi <arata.takenouchi.1230@gmail.com>
2025-01-05 22:58:22 +0900

    lesson26-2

--- lesson25.py
+++ lesson25.py
@@ -1,2 +1,17 @@
-with open('test.txt', 'w') as f:
-    f.write('Test\n')
+s = """\
+AAA
+BBB
+CCC
+DDD
+"""
+# with open('test.txt', 'w') as f:
+#     f.write(s)
+
+with open('test.txt', 'r') as f:
+    # print(f.read())
+    while True:
+        chunk = 2
+        line = f.read(chunk)
+        print(line)
+        if not line:
+            break
