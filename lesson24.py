1d636f74e6ab9adac9040a972d57e4270f4470e9
arata-takenouchi <arata.takenouchi.1230@gmail.com>
2025-01-05 21:07:14 +0900

    lesson24-5

--- lesson24.py
+++ lesson24.py
@@ -1,20 +1,31 @@
 class Car(object):
+    def __init__(self, model=None):
+        self.model = model
     def run(self):
         print('run')
 
 class ToyotaCar(Car):
-    pass
+    def run(self):
+        print('fast')
 
-class TeslaCar():
+class TeslaCar(Car):
+    def __init__(self, model='Model S', enable_auto_run=False):
+        # self.model = model
+        super().__init__(model)
+        enable_auto_run = enable_auto_run
+    def run(self):
+        print('super fast')
     def auto_run(self):
         print('auto run')
 
 car = Car()
 car.run()
 
-toyota_car = ToyotaCar()
+toyota_car = ToyotaCar('Lexus')
+print(toyota_car.model)
 toyota_car.run()
 
-tesla_car = TeslaCar()
-tesla_car.auto_run()
+tesla_car = TeslaCar('Model S')
+print(tesla_car.model)
 tesla_car.run()
+tesla_car.auto_run()
