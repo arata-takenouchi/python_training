cf52938416d46a3f8f47f5138c86dc002f4040c5
arata-takenouchi <arata.takenouchi.1230@gmail.com>
2025-01-08 01:20:57 +0900

    lesson27

---
+++ lesson26.py
@@ -0,0 +1,19 @@
+x = 1;
+y = 2;
+
+x = 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+def test_func(x, y, z,
+              huashuahuashausuahsauhsuahsauhusahusahusahsuhsauhsauhaushushahsuahsauhsaushaushaushaushau):
+    """
+
+    :param x:
+    :param y:
+    :param z:
+    :param huashuahuashausuahsauhsuahsauhusahusahusahsuhsauhsauhaushushahsuahsauhsaushaushaushaushsasiashaiuhsauhsauhsushuasau:
+    :return:
+
+    See details at: https://huppost.com/iajsisjaisjaisjaijiasaijiajsaijaisjaisjaisjaijsiajsisjisaisjiajsiajsiasjaijsaissajaijia
+    """
+
+if (x and y):
+    print('test')
