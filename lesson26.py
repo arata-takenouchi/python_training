9a797da923c72189f172827757093e6edb3e2a6c
rundle-takenouchi-a <arata.takenouchi@rundle.co.jp>
2025-01-08 23:44:11 +0900

    lesson27-2

--- lesson26.py
+++ lesson26.py
@@ -17,3 +17,37 @@ def test_func(x, y, z,
 
 if (x and y):
     print('test')
+
+    x = {'test': 'sss'}
+
+    x, y = y, x
+    x=y
+    x    = 100
+    yyyy = 1
+
+word = 'hello'
+word2 = '!'
+
+new_word = f'{word}{word2}'
+new_word = word + word2
+
+long_word = ""
+for word in ['gsgssgs', 'hahshsh', 'jdnsidnsd']:
+    long_word += word
+
+long_word = []
+for word in ['gsgssgs', 'hahshsh', 'jdnsidnsd']:
+    long_word.append(word)
+new_long_word = ''.join(long_word)
+
+print('ksksks')
+
+f"hshshshs {'test'} huhuhuhu"
+
+if x:
+    print('exit')
+else:
+    print('else')
+
+if x: print('exit')
+else: print('else')
