42917aca215f2cffbaf4d135ab35cb2356c455c2
arata-takenouchi <arata.takenouchi.1230@gmail.com>
2025-01-10 07:52:11 +0900

    lesson27-4

---
+++ lesson28.py
@@ -0,0 +1,53 @@
+x = 1;
+y = 2;
+
+x = 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+def test_func(x, y, z,
+              huashuahuashausuahsauhsuahsauhusahusahusahsuhsauhsauhaushushahsuahsauhsaushaushaushaushau):
+    """
+
+    :param x:
+    :param y:
+    :param z:
+    :param huashuahuashausuahsauhsuahsauhusahusahusahsuhsauhsauhaushushahsuahsauhsaushaushaushaushsasiashaiuhsauhsauhsushuasau:
+    :return:
+
+    See details at: https://huppost.com/iajsisjaisjaisjaijiasaijiajsaijaisjaisjaisjaijsiajsisjisaisjiajsiajsiasjaijsaissajaijia
+    """
+
+if (x and y):
+    print('test')
+
+    x = {'test': 'sss'}
+
+    x, y = y, x
+    x=y
+    x    = 100
+    yyyy = 1
+
+word = 'hello'
+word2 = '!'
+
+new_word = f'{word}{word2}'
+new_word = word + word2
+
+long_word = ""
+for word in ['gsgssgs', 'hahshsh', 'jdnsidnsd']:
+    long_word += word
+
+long_word = []
+for word in ['gsgssgs', 'hahshsh', 'jdnsidnsd']:
+    long_word.append(word)
+new_long_word = ''.join(long_word)
+
+print('ksksks')
+
+f"hshshshs {'test'} huhuhuhu"
+
+if x:
+    print('exit')
+else:
+    print('else')
+
+if x: print('exit')
+else: print('else')
--- main.py
+++ main.py
@@ -1,34 +1,78 @@
-import os
-import sys
+def base(x):
+    def plus(y):
+        return x + y
+    return plus
+plus = base(10)
+print(plus(10))
+print(plus(30))
 
-import flask
+i = 0
+def add_num():
+    def plus(y):
+        return i + y
+    return plus
+i = 10
+plus = add_num()
+print(plus(10))
+print(plus(30))
+i = 100
+print(plus(30))
 
-import roboter.contlorller.conversation
-# from roboter.contlorller import conversation
+# import os
+# import sys
+#
+# import flask
+#
+# import roboter.contlorller.conversation
+# # from roboter.czontlorller import conversation
+#
+# import settings
 
-import settings
+# class MainError(Exception):
+#     pass
+#
+# def main():
+#     try:
+#         roboter.contlorller.conversation.talk_about_restaulant
+#     except Exception as e:
+#         print(e)
+#     raise MainError('error')
+#
+# x = [(i, x) for i in [1,2,3,4] for x in [1,2,3]]
+#
+# d = {'key1': 'value1', 'key2', 'value2'}
+# if 'key1' in d:
+#     print('test')
+#
+# if 'key1' in d.keys():
+#     print('test')
+#
+# for name, count in ranking.items():
+#     print(name, count)
+#
+# if __name__ == 'main':
+#     main()
 
-class MainError(Exception):
-    pass
+# def t():
+#     # num = []
+#     for i in range(10):
+#         yield i
+#     #     num.append(i)
+#     # return num
+#
+# for i in t():
+#     print(i)
 
-def main():
-    try:
-        roboter.contlorller.conversation.talk_about_restaulant
-    except Exception as e:
-        print(e)
-    raise MainError('error')
+# def other_func(f):
+#     print(f(10))
+#
+# def test_func(x):
+#     return x * 2
+#
+# other_func(test_func)
+# other_func(lambda x: x * 2)
 
-x = [(i, x) for i in [1,2,3,4] for x in [1,2,3]]
-
-d = {'key1': 'value1', 'key2', 'value2'}
-if 'key1' in d:
-    print('test')
-
-if 'key1' in d.keys():
-    print('test')
-
-for name, count in ranking.items():
-    print(name, count)
-
-if __name__ == 'main':
-    main()
+# y = None
+# y = 'gagagag'
+# x = 1 if y else 2
+# print(x)
