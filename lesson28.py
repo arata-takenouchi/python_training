f10404272f6798694ee5e6e7a9a1b5c2e5f8c1f9
arata-takenouchi <arata.takenouchi.1230@gmail.com>
2025-01-11 23:40:54 +0900

    lesson28-2

--- lesson28.py
+++ lesson28.py
@@ -1,53 +1,26 @@
-x = 1;
-y = 2;
+"""
+web_server
+host: 127.0.0.1
+port: 80
 
-x = 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
-def test_func(x, y, z,
-              huashuahuashausuahsauhsuahsauhusahusahusahsuhsauhsauhaushushahsuahsauhsaushaushaushaushau):
-    """
+db_server
+    host: 127.0.0.1
+    port: 3306
+"""
+import yaml
 
-    :param x:
-    :param y:
-    :param z:
-    :param huashuahuashausuahsauhsuahsauhusahusahusahsuhsauhsauhaushushahsuahsauhsaushaushaushaushsasiashaiuhsauhsauhsushuasau:
-    :return:
+with open('config.yml', 'w') as yaml_file:
+    yaml.dump({
+        'web_server': {
+            'host': '127.0.0.1',
+            'port': 80
+        },
+        'db_server': {
+            'host': '127.0.0.1',
+            'port': 3306
+        }
+    }, yaml_file)
 
-    See details at: https://huppost.com/iajsisjaisjaisjaijiasaijiajsaijaisjaisjaisjaijsiajsisjisaisjiajsiajsiasjaijsaissajaijia
-    """
-
-if (x and y):
-    print('test')
-
-    x = {'test': 'sss'}
-
-    x, y = y, x
-    x=y
-    x    = 100
-    yyyy = 1
-
-word = 'hello'
-word2 = '!'
-
-new_word = f'{word}{word2}'
-new_word = word + word2
-
-long_word = ""
-for word in ['gsgssgs', 'hahshsh', 'jdnsidnsd']:
-    long_word += word
-
-long_word = []
-for word in ['gsgssgs', 'hahshsh', 'jdnsidnsd']:
-    long_word.append(word)
-new_long_word = ''.join(long_word)
-
-print('ksksks')
-
-f"hshshshs {'test'} huhuhuhu"
-
-if x:
-    print('exit')
-else:
-    print('else')
-
-if x: print('exit')
-else: print('else')
+with open('config.yml', 'r') as yaml_file:
+    data = yaml.load(yaml_file)
+    print(data, type(data))
