9e2fdd4c777eaf20260f6098a08ae64170531bdb
arata-takenouchi <arata.takenouchi.1230@gmail.com>
2025-01-12 23:25:32 +0900

    lesson28-10

--- lesson28.py
+++ lesson28.py
@@ -1,18 +1,25 @@
-import logging
+from email import message
+import smtplib
 
-logger = logging.getLogger(__name__)
+import config
 
-logger.error('api call is failed')
+smtp_host = 'smtp.live.com'
+smtp_port = 587
+from_email = 'xxx@hotmail.com'
+to_email = 'arata.takenouchi.1230@gmail.com'
+username = 'arata.takenouchi.1230@gmail.com'
+password = '97246242a1'
 
-logger.error({
-    'action': 'create',
-    'status' : 'fail',
-    'message': 'api call is failed'
-})
+msg = message.EmailMessage()
+msg.set_content('Test email')
+msg['Subject'] = 'Test email sub'
+msg['From'] = from_email
+msg['To'] = to_email
 
-logger.info({
-    'action': 'save',
-    'csv_file': self.csv_file,
-    'force': True,
-    'status': 'run'
-})
+server = smtplib.SMTP(smtp_host, smtp_port)
+server.ehlo()
+server.starttls()
+server.ehlo()
+server.login(username, password)
+server.send_message(msg)
+server.quit()
