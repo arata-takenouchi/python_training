0160afffe2e43a85608f8c6100ced3a3a734943e
arata-takenouchi <arata.takenouchi.1230@gmail.com>
2025-01-12 21:48:28 +0900

    lesson28-3

--- lesson28.py
+++ lesson28.py
@@ -1,26 +1,13 @@
-"""
-web_server
-host: 127.0.0.1
-port: 80
+import logging
 
-db_server
-    host: 127.0.0.1
-    port: 3306
-"""
-import yaml
+logging.basicConfig(filename='test.log', level=logging.DEBUG)
 
-with open('config.yml', 'w') as yaml_file:
-    yaml.dump({
-        'web_server': {
-            'host': '127.0.0.1',
-            'port': 80
-        },
-        'db_server': {
-            'host': '127.0.0.1',
-            'port': 3306
-        }
-    }, yaml_file)
+# logging.critical('critical')
+# logging.error('error')
+# logging.warning('warning')
+# logging.info('info')
+# logging.debug('debug')
 
-with open('config.yml', 'r') as yaml_file:
-    data = yaml.load(yaml_file)
-    print(data, type(data))
+# logging.info('info %s %s' % ('test', 'test2'))
+logging.info('info %s %s', 'test', 'test2')
+
