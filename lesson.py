4384b2e02bad1c96b16269ec55bad517998da8f2
arata-takenouchi <arata.takenouchi.1230@gmail.com>
2025-01-13 21:44:58 +0900

    lesson29-5

--- lesson.py
+++ lesson.py
@@ -1,22 +1,45 @@
-import pickle
+import datetime
+from pymongo import MongoClient
 
-class T(object):
-    def __init__(self, name):
-        self.name = name
+client = MongoClient('mongodb://localhost:27017/')
+db = client['test_database']
 
-data = {
-    'a': [1,2,3],
-    'b': ('test', 'test'),
-    'c': {'key': 'value'},
-    'd': T('test')
+stack1 = {
+    'name': 'customer1',
+    'pip': ['python', 'java', 'go'],
+    'info': {'os': 'mac'},
+    'date': datetime.datetime.now(datetime.UTC)
 }
 
-with open('data.pickle', 'wb') as f:
-    pickle.dump(data, f)
+stack2 = {
+    'name': 'customer2',
+    'pip': ['python', 'java'],
+    'info': {'os': 'windows'},
+    'date': datetime.datetime.now(datetime.UTC)
+}
 
-with open('data.pickle', 'rb') as f:
-    data_loaded = pickle.load(f)
-    print(type(data_loaded['a']))
-    print(type(data_loaded['b']))
-    print(type(data_loaded['c']))
-    print(type(data_loaded['d']))
+db_stacks = db.stacks
+# stack_id = db_stacks.insert_one(stack1).inserted_id
+# print(stack_id, type(stack_id))
+# print('####')
+from bson.objectid import ObjectId
+# str_stack_id = '6785035dc47e400254dc3bf0'
+# print(db_stacks.find_one({'_id': ObjectId(str_stack_id)}))
+
+# stack_id = db_stacks.insert_one(stack2).inserted_id
+# print(stack_id, type(stack_id))
+#
+# for stack in db_stacks.find():
+#     print(stack)
+
+# now = datetime.datetime.now(datetime.UTC)
+# for stack in db_stacks.find({'data': {'$lt': now}}):
+#     print(stack)
+
+# db_stacks.find_one_and_update(
+#     {'name': 'customer1'}, {'$set': {'name': 'YYY'}}
+# )
+# print(db_stacks.find_one({'name': 'YYY'}))
+
+db_stacks.delete_one({'name': 'YYY'})
+print(db_stacks.find_one({'name': 'YYY'}))
