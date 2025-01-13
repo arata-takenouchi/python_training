3145146949ae8d38555cc04f0dfcddbb4ffde783
arata-takenouchi <arata.takenouchi.1230@gmail.com>
2025-01-13 20:11:42 +0900

    lesson29-3

--- lesson29.py
+++ lesson29.py
@@ -1,9 +1,37 @@
-import dbm
+import sqlite3
+import time
+import memcache
 
-with dbm.open('cache', 'c') as db:
-    # db['key1'] = 'value1'
-    # db['key2'] = 'value2'
-    db['key3'] = 1
+db = memcache.Client(['127.0.0.1:11211'])
 
-with dbm.open('cache', 'r') as db:
-    print(db.get('key1'))
+# db.set('web_page', 'value1', time=3)
+# time.sleep(1)
+# print(db.get('web_page'))
+
+# db.set('counter', 0)
+# db.incr('counter', 1)
+# db.incr('counter', 1)
+# print(db.get('counter'))
+
+conn = sqlite3.connect('test_sqlite2.db')
+curs = conn.cursor()
+# curs.execute('CREATE TABLE persons(employ_id INTEGER PRIMARY KEY AUTOINCREMENT, name STRING)')
+# curs.execute('INSERT INTO persons(name) values("Mike")')
+# conn.commit()
+# conn.close()
+
+def get_employ_id(name):
+    employ_id = db.get(name)
+    if employ_id:
+        return employ_id
+    curs.execute(
+        'SELECT * FROM persons WHERE name = "{}".format(name)'
+    )
+    person = curs.fetchone()
+    if not person:
+        raise Exception('No employ')
+    employ_id, name = person
+    db.set(name, employ_id, time=60)
+    return employ_id
+
+print(get_employ_id("Mike"))
