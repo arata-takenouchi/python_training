9de9a4fedfe7eb650be5467fa62ab606a5a80286
arata-takenouchi <arata.takenouchi.1230@gmail.com>
2025-01-15 00:22:45 +0900

    lesson29-10

--- lesson.py
+++ lesson.py
@@ -1,5 +1,29 @@
-import http.server
-import socketserver
+from flask import Flask
+from flask import g
+from flask import render_template
+from flask import request
+from flask import Response
 
-with socketserver.TCPServer(('127.0.0.1', 8000), http.server.SimpleHTTPRequestHandler) as httpd:
-    httpd.serve_forever()
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'top'
+
+@app.route('/hello')
+@app.route('/hello/<username>')
+def hello_world2(username=None):
+    # return f'hello world! {username}'
+    return render_template('hello.html', username=username)
+
+@app.route('/post', methods=['GET', 'POST', 'PUT', 'DELETE'])
+def show_post():
+    return str(request.values['username'])
+
+def main():
+    app.debug = True
+    app.run()
+    # app.run(host='127.0.0.1', port=5000)
+
+if __name__ == '__main__':
+    main()
