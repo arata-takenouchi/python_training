b835dc6ae720560130e35950878b1f6a10bb6c04
arata-takenouchi <arata.takenouchi.1230@gmail.com>
2025-01-16 00:03:14 +0900

    lesson30-3

--- test_calculation.py
+++ test_calculation.py
@@ -5,7 +5,12 @@ import calculation
 class CalTest(unittest.TestCase):
     def test_add_num_and_double(self):
         cal = calculation.Cal()
-        self.assertAlmostEqual(cal.add_num_and_double(1, 1),5)
+        self.assertAlmostEqual(cal.add_num_and_double(1, 1),4)
+
+    def test_add_num_and_double_raise(self):
+        cal = calculation.Cal()
+        with self.assertRaises(ValueError):
+            cal.add_num_and_double('1', '1')
 
 # if __name__ == '__main__':
 #     unittest.main()
