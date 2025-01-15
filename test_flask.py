426561f045bb1d9ea1fe8ab5576142226f58926b
arata-takenouchi <arata.takenouchi.1230@gmail.com>
2025-01-15 23:38:38 +0900

    lesson29-11

--- test_flask.py
+++ test_flask.py
@@ -1,6 +1,16 @@
 import requests
 
-r = requests.delete(
-    'http://127.0.0.1:5000/post', data={'username': 'Mike'}
-)
+r = requests.get('http://127.0.0.1:5000/employee/arata')
+print(r.text)
+
+r = requests.post('http://127.0.0.1:5000/employee', data={'name': 'arata'})
+print(r.text)
+
+r = requests.delete('http://127.0.0.1:5000/employee', data={'name': 'arata'})
+print(r.text)
+
+r = requests.put('http://127.0.0.1:5000/employee', data={
+    'name': 'arata',
+    'new_name': 'takenouchi'
+})
 print(r.text)
