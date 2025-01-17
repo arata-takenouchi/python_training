8d48413296174514145b376f366d3dff6de2b851
arata-takenouchi <arata.takenouchi.1230@gmail.com>
2025-01-17 23:04:57 +0900

    lesson30-4

--- test_calculation.py
+++ test_calculation.py
@@ -3,14 +3,22 @@ import unittest
 import calculation
 
 class CalTest(unittest.TestCase):
+    def setUp(self):
+        print('setup')
+        self.cal = calculation.Cal()
+
+    def tearDown(self):
+        print('clean up')
+        del self.cal
+
     def test_add_num_and_double(self):
-        cal = calculation.Cal()
-        self.assertAlmostEqual(cal.add_num_and_double(1, 1),4)
+        # cal = calculation.Cal()
+        self.assertAlmostEqual(self.cal.add_num_and_double(1, 1),4)
 
     def test_add_num_and_double_raise(self):
-        cal = calculation.Cal()
+        # cal = calculation.Cal()
         with self.assertRaises(ValueError):
-            cal.add_num_and_double('1', '1')
+            self.cal.add_num_and_double('1', '1')
 
 # if __name__ == '__main__':
 #     unittest.main()
