656bda25b1ad38d6bdd6b7d4302d2c11e03aa691
arata-takenouchi <arata.takenouchi.1230@gmail.com>
2025-01-18 23:43:59 +0900

    lesson30-10

---
+++ conftest.py
@@ -0,0 +1,3 @@
+def pytest_addoption(parser):
+    # parser.addoption('--os-name', defalut='linux', help='os name')
+    parser.addoption('--os-name', help='os name')
--- test_calculation.py
+++ test_calculation.py
@@ -1,10 +1,5 @@
-# import unittest
-
 import pytest
-
 import calculation
-
-is_release = True
 
 class TestCal(object):
 
@@ -13,24 +8,11 @@ class TestCal(object):
         print('start')
         cls.cal = calculation.Cal()
 
-    @classmethod
-    def teardown_method(cls):
-        print('end')
-        del cls.cal
-
-    def setup_method(self, method):
-        print(f'method={method.__name__}')
-        # self.cal = calculation.Cal()
-
-    def teardown_method(self, method):
-        print(f'method={method.__name__}')
-        # del self.cal
-
-    def test_add_num_and_double(self):
+    def test_add_num_and_double(self, request):
+        os_name = request.config.getoption('--os-name')
+        print(os_name)
+        if os_name == 'mac':
+            print('ls')
+        elif os_name == 'windows':
+            print('dir')
         assert self.cal.add_num_and_double(1, 1) == 4
-
-    # @pytest.mark.skip(reason='skip')
-    @pytest.mark.skipif(is_release==True, reason='skip')
-    def test_add_num_and_double_raise(self):
-        with pytest.raises(ValueError):
-            self.cal.add_num_and_double('1', '1')
