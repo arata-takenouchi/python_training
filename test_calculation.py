38c52f24e57b1c5ae9651ef1437ea62a93074188
arata-takenouchi <arata.takenouchi.1230@gmail.com>
2025-01-18 23:06:04 +0900

    lesson30-6

--- test_calculation.py
+++ test_calculation.py
@@ -1,28 +1,39 @@
-import unittest
+# import unittest
+
+import pytest
 
 import calculation
 
-release_name = 'lesson2'
+# def test_add_num_and_double():
+#     cal = calculation.Cal()
+#     assert cal.add_num_and_double(1, 1) == 4
 
-class CalTest(unittest.TestCase):
-    def setUp(self):
-        print('setup')
-        self.cal = calculation.Cal()
-
-    def tearDown(self):
-        print('clean up')
-        del self.cal
-
-    # @unittest.skip('skip')
-    @unittest.skipIf(release_name=='lesson', 'skip')
+class TestCal(object):
     def test_add_num_and_double(self):
-        # cal = calculation.Cal()
-        self.assertAlmostEqual(self.cal.add_num_and_double(1, 1),4)
+        cal = calculation.Cal()
+        assert cal.add_num_and_double(1, 1) == 4
 
-    def test_add_num_and_double_raise(self):
-        # cal = calculation.Cal()
-        with self.assertRaises(ValueError):
-            self.cal.add_num_and_double('1', '1')
+# release_name = 'lesson2'
+#
+# class CalTest(unittest.TestCase):
+#     def setUp(self):
+#         print('setup')
+#         self.cal = calculation.Cal()
+#
+#     def tearDown(self):
+#         print('clean up')
+#         del self.cal
+#
+#     # @unittest.skip('skip')
+#     @unittest.skipIf(release_name=='lesson', 'skip')
+#     def test_add_num_and_double(self):
+#         # cal = calculation.Cal()
+#         self.assertAlmostEqual(self.cal.add_num_and_double(1, 1),4)
+#
+#     def test_add_num_and_double_raise(self):
+#         # cal = calculation.Cal()
+#         with self.assertRaises(ValueError):
+#             self.cal.add_num_and_double('1', '1')
 
 # if __name__ == '__main__':
 #     unittest.main()
