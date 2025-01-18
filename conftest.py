abea1e812f03e663eea752134b05ccb71b296373
arata-takenouchi <arata.takenouchi.1230@gmail.com>
2025-01-19 00:18:57 +0900

    lesson30-12

--- conftest.py
+++ conftest.py
@@ -1,3 +1,13 @@
+import os
+import pytest
+
+@pytest.fixture
+def csv_file(tmpdir):
+    with open(os.path.join(tmpdir, 'test.csv'), 'w+') as c:
+        print('before test')
+        yield c
+        print('after test')
+
 def pytest_addoption(parser):
     # parser.addoption('--os-name', defalut='linux', help='os name')
     parser.addoption('--os-name', help='os name')
