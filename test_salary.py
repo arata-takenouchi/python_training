93f673cc816d282f0ab3be2c24ef7065bc967769
arata-takenouchi <arata.takenouchi.1230@gmail.com>
2025-01-21 00:44:05 +0900

    lesson30-15

--- test_salary.py
+++ test_salary.py
@@ -8,3 +8,14 @@ class TestSalary(unittest.TestCase):
         s = salary.Salary(year=2017)
         s.bonus_api.bonus_price = MagicMock(return_value=1)
         self.assertEqual(s.calculation_salary(), 101)
+        s.bonus_api.bonus_price.assert_called()
+        s.bonus_api.bonus_price.assert_called_once()
+        s.bonus_api.bonus_price.assert_called_with(year=2017)
+        s.bonus_api.bonus_price.assert_called_once_with(year=2017)
+        self.assertEqual(s.bonus_api.bonus_price.call_count, 1)
+
+    def test_calculation_no_salary(self):
+        s = salary.Salary(year=2050)
+        s.bonus_api.bonus_price = MagicMock(return_value=0)
+        self.assertEqual(s.calculation_salary(), 100)
+        s.bonus_api.bonus_price.assert_not_called()
