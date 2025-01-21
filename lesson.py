9d6329099590ff87f7a1b99806c371953bd5b3cf
arata-takenouchi <arata.takenouchi.1230@gmail.com>
2025-01-21 23:55:21 +0900

    lesson31-2

--- lesson.py
+++ lesson.py
@@ -9,14 +9,16 @@ def worker1():
     time.sleep(5)
     logging.debug('end')
 
-def worker2():
+def worker2(x, y=1):
     logging.debug('start')
+    logging.debug(x)
+    logging.debug(y)
     time.sleep(5)
     logging.debug('end')
 
 if __name__ == '__main__':
-    t1 = threading.Thread(target=worker1)
-    t2 = threading.Thread(target=worker2)
+    t1 = threading.Thread(name='rename worker', target=worker1)
+    t2 = threading.Thread(target=worker2, args=(100, ), kwargs={'y': 200})
     t1.start()
     t2.start()
     print('started')
