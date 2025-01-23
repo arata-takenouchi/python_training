a3c537a18ca03419345c2e4d889a01a3ac96e6eb
arata-takenouchi <arata.takenouchi.1230@gmail.com>
2025-01-23 23:22:53 +0900

    lesson31-4

--- lesson.py
+++ lesson.py
@@ -15,10 +15,19 @@ def worker2():
     logging.debug('end')
 
 if __name__ == '__main__':
-    t1 = threading.Thread(target=worker1)
-    t1.daemon = True
-    t2 = threading.Thread(target=worker2)
-    t1.start()
-    t2.start()
-    print('started')
-    t1.join()
+    # threads = []
+    for _ in range(5):
+        t = threading.Thread(target=worker1)
+        t.daemon = True
+        t.start()
+        # threads.append(t)
+    for thread in threading.enumerate():
+        if thread is threading.current_thread():
+            print(thread)
+            continue
+        thread.join()
+    # t2 = threading.Thread(target=worker2)
+    # t1.start()
+    # t2.start()
+    # print('started')
+    # t1.join()
