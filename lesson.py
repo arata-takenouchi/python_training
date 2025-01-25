84caca869b829012bb01b64c67f237b6c0379c8f
arata-takenouchi <arata.takenouchi.1230@gmail.com>
2025-01-25 20:04:41 +0900

    lesson31-18

--- lesson.py
+++ lesson.py
@@ -12,28 +12,14 @@ import time
 # logging.basicConfig(level=logging.DEBUG, format='%(threadName)s: %(message)s')
 logging.basicConfig(level=logging.DEBUG, format='%(processName)s: %(message)s')
 
-def worker1(d, lock):
-    with lock:
-        i = d['x']
-        time.sleep(2)
-        d['x'] = i + 1
-    logging.debug(d)
-
-def worker2(d, lock):
-    with lock:
-        i = d['x']
-        d['x'] = i + 1
-    logging.debug(d)
+def f(conn):
+    conn.send(['test'])
+    time.sleep(5)
+    conn.close()
 
 if __name__ == '__main__':
-    d = {'x': 0}
-    lock = Lock()
-    # t1 = threading.Thread(target=worker1, args=(d, lock))
-    # t2 = threading.Thread(target=worker1, args=(d, lock))
-    t1 = multiprocessing.Process(target=worker1, args=(d, lock))
-    t2 = multiprocessing.Process(target=worker2, args=(d, lock))
-    t1.start()
-    t2.start()
-    t1.join()
-    t2.join()
-    logging.debug(d)
+    parent_conn, child_conn = multiprocessing.Pipe()
+    p = multiprocessing.Process(target=f, args=(parent_conn,))
+    p.start()
+    # p.join()
+    logging.debug(child_conn.recv())
