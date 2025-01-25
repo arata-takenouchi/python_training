53f842c72b727735bc7715c77e3c790dff736169
arata-takenouchi <arata.takenouchi.1230@gmail.com>
2025-01-25 19:01:58 +0900

    lesson31-13

--- lesson.py
+++ lesson.py
@@ -1,29 +1,28 @@
+from multiprocessing import (
+    Process,
+    Lock, RLock, Semaphore, Queue, Event, Condition, Barrier,
+    Value, Array, Pipe, Manager
+)
+
 import logging
-import queue
-import threading
+import multiprocessing
 import time
 
-logging.basicConfig(level=logging.DEBUG, format='%(threadName)s: %(message)s')
+logging.basicConfig(level=logging.DEBUG, format='%(processName)s: %(message)s')
 
-def worker1(barrier):
-    r = barrier.wait()
-    logging.debug(f'num={r}')
-    while True:
-        logging.debug('start')
-        time.sleep(2)
-        logging.debug('end')
+def worker1(i):
+    logging.debug('start')
+    logging.debug(i)
+    logging.debug('end')
 
-def worker2(barrier):
-    r = barrier.wait()
-    logging.debug(f'num={r}')
-    while True:
-        logging.debug('start')
-        time.sleep(2)
-        logging.debug('end')
+def worker2(i):
+    logging.debug('start')
+    logging.debug(i)
+    logging.debug('end')
 
 if __name__ == '__main__':
-    event = threading.Barrier(2)
-    t1 = threading.Thread(target=worker1, args=(event,))
-    t2 = threading.Thread(target=worker2, args=(event,))
+    i = 10
+    t1 = multiprocessing.Process(target=worker1, args=(i,))
+    t2 = multiprocessing.Process(name='renamed worker2', target=worker2, args=(i,))
     t1.start()
     t2.start()
