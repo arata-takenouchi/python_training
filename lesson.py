1c1fcfc46d6f92648d88e3b30bf83a91d120dab3
arata-takenouchi <arata.takenouchi.1230@gmail.com>
2025-01-25 19:42:34 +0900

    lesson31-16

--- lesson.py
+++ lesson.py
@@ -14,14 +14,16 @@ def worker1(i):
     logging.debug('start')
     time.sleep(5)
     logging.debug('end')
-    return i
+    return i * 2
 
 if __name__ == '__main__':
     with multiprocessing.Pool(3) as p:
-        logging.debug(p.apply(worker1, (200,)))
-        logging.debug('executed apply')
-        p1 = p.apply_async(worker1, (100,))
-        p2 = p.apply_async(worker1, (100,))
+        r = p.imap(worker1, [100, 200])
         logging.debug('executed')
-        logging.debug(p1.get())
-        logging.debug(p2.get())
+        logging.debug([i for i in r])
+
+        # p1 = p.apply_async(worker1, (100,))
+        # p2 = p.apply_async(worker1, (100,))
+        # logging.debug('executed')
+        # logging.debug(p1.get())
+        # logging.debug(p2.get())
