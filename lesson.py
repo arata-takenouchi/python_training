25a4663a74c8c573987d90ccc13e9f2b98b25529
arata-takenouchi <arata.takenouchi.1230@gmail.com>
2025-01-25 18:53:45 +0900

    lesson31-12

--- lesson.py
+++ lesson.py
@@ -5,31 +5,25 @@ import time
 
 logging.basicConfig(level=logging.DEBUG, format='%(threadName)s: %(message)s')
 
-def worker1(condition):
-    with condition:
-        condition.wait()
+def worker1(barrier):
+    r = barrier.wait()
+    logging.debug(f'num={r}')
+    while True:
         logging.debug('start')
-        time.sleep(3)
+        time.sleep(2)
         logging.debug('end')
 
-def worker2(condition):
-    with condition:
-        condition.wait()
+def worker2(barrier):
+    r = barrier.wait()
+    logging.debug(f'num={r}')
+    while True:
         logging.debug('start')
-        time.sleep(3)
+        time.sleep(2)
         logging.debug('end')
-
-def worker3(condition):
-    with condition:
-        logging.debug('start')
-        logging.debug('end')
-        condition.notifyAll()
 
 if __name__ == '__main__':
-    event = threading.Condition()
+    event = threading.Barrier(2)
     t1 = threading.Thread(target=worker1, args=(event,))
     t2 = threading.Thread(target=worker2, args=(event,))
-    t3 = threading.Thread(target=worker3, args=(event,))
     t1.start()
     t2.start()
-    t3.start()
