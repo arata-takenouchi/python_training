0c5724254a6ffcc2bd6fbc0f010cb7095c411ff8
arata-takenouchi <arata.takenouchi.1230@gmail.com>
2025-01-27 23:07:29 +0900

    lesson32-2

--- docker/ops/fab_project/test_fabfile.py
+++ docker/ops/fab_project/test_fabfile.py
@@ -1,75 +1,38 @@
 # from fabric.api import run, env, roles, task, parallel, execute, runs_once
 from fabric.api import *
+from fabric.colors import *
 from fabric.contrib.files import *
 
-env.hosts = ['root@172.16.200.101:22', 'root@172.16.200.102:24']
+env.hosts = ['root@172.16.200.101:22']
 # env.hosts = ['root@172.16.200.102:24']
 env.passwords = {
 	'root@172.16.200.101:22': 'root',
 	'root@172.16.200.102:24': 'root',
 }
 env.roledefs = {
+	'common': ['root@172.16.200.101:22'],
 	'web': ['root@172.16.200.101:22'],
-	'db': ['root@172.16.200.102:24'],
 }
 
+@task
+@roles('common')
+def install_packages():
+	run('apt-get install -y software-properties-common')
+	run('add-apt-repository universe')
+	run('apt-get update')
+	run('apt-get install -y python-setuptools')
+	run('easy_install pip')
+
+@task
 @roles('web')
-def host_type():
-	run('uname -s')
-
-@roles('db')
-def host_files():
-	run('ls -al')
-
-def all_files():
-
+def deploy_web():
+	run('apt-get install -y supervisor')
+	run('apt-get install -y gunicorn')
+	run('pip install Flask--0.12.2')
 
 @task
-def go():
-	all_files()
-	# run('ls -al')
+def deploy_web_server():
+	install_packages()
+	deploy_web()
+	print(green('success'))
 
-@task(default=True)
-def who():
-	run('whoami')
-
-@task
-@parallel(pool_size=2)
-def para():
-	run('ls -al')
-
-@task
-def argtest(arg1, arg2):
-	print(arg1, arg2)
-
-def test():
-	return run('ls -al')
-
-@task
-def org():
-	r = execute(test)
-
-@runs_once
-def db_init():
-	print('init')
-
-@task
-def deploy_db():
-	db_init()
-	db_init()
-
-@task
-def clean_and_upload():
-	local('ls -al')
-	put('test_fabfile.py', '/tmp/test_fabfile.py')
-	with cd('/tmp'):
-		run('pwd')
-		run('ls -al')
-		print(exists('/tmp/test_fabfile.py'))
-
-import db.checking
-
-@task
-def split_test():
-	r = execute(db.checking.check())
-	print(r)
