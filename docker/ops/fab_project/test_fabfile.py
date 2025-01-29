b6cd7b4576c9989d48e0f878ab35a94614ba304e
arata-takenouchi <arata.takenouchi.1230@gmail.com>
2025-01-29 23:49:34 +0900

    lesson32-2

--- docker/ops/fab_project/test_fabfile.py
+++ docker/ops/fab_project/test_fabfile.py
@@ -3,6 +3,8 @@ from fabric.api import *
 from fabric.colors import *
 from fabric.contrib.files import *
 
+WEB_DIR = '/root/work'
+
 env.hosts = ['root@172.16.200.101:22']
 # env.hosts = ['root@172.16.200.102:24']
 env.passwords = {
@@ -29,6 +31,12 @@ def deploy_web():
 	run('apt-get install -y supervisor')
 	run('apt-get install -y gunicorn')
 	run('pip install Flask--0.12.2')
+	if not exists(WEB_DIR):
+		run(f'mkdir {WEB_DIR}')
+
+	put('roles/web/files/hello.py', '/root/work/hello.py')
+	put('roles/web/files/app.conf', '/etc/supervisor/conf.d/app.conf')
+	run('service supervisor restart')
 
 @task
 def deploy_web_server():
