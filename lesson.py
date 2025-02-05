4a9934f0e3c741177f6b386b2988e00a04c85301
arata-takenouchi <arata.takenouchi.1230@gmail.com>
2025-02-06 00:22:40 +0900

    lesson36-3

--- lesson.py
+++ lesson.py
@@ -1,16 +1,20 @@
-# def g_hello():
-#     yield 'hello 1'
-#     yield 'hello 2'
-#     yield 'hello 3'
+def s_hello():
+    yield 'hello 1'
+    yield 'hello 2'
+    yield 'hello 3'
+    return 'done'
 
 def g_hello():
-    r = yield 'hello'
-    yield r
+    while True:
+        r = yield from s_hello()
+        yield r
 
 g = g_hello()
 print(next(g))
-# print(next(g))
-print(g.send('plus'))
-
-# for w in g_hello():
-#     print(w)
+print(next(g))
+print(next(g))
+print(next(g))
+print(next(g))
+print(next(g))
+print(next(g))
+print(next(g))
