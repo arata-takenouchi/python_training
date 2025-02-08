6202466dea9b93871771767e6c83fe827581cdb0
arata-takenouchi <arata.takenouchi.1230@gmail.com>
2025-02-08 21:54:22 +0900

    lesson36-7

--- lesson.py
+++ lesson.py
@@ -2,22 +2,30 @@ import asyncio
 
 loop = asyncio.get_event_loop()
 
-async def worker1(lock):
-    print('worker1 start')
-    with await lock:
-        print('worker1 got lock')
+async def worker1(condition):
+    with await condition:
+        print('worker1 start')
+        print('worker1 got condition')
         await asyncio.sleep(3)
-    print('worker1 end')
+        print('worker1 end')
 
-async def worker2(lock):
-    print('worker2 start')
-    with await lock:
-        print('worker2 got lock')
+async def worker2(condition):
+    with await condition:
+        await condition.wait()
+        print('worker2 start')
+        print('worker2 got condition')
         await asyncio.sleep(3)
-    print('worker2 end')
+        print('worker2 end')
 
-lock = asyncio.Lock()
+async def worker3(condition):
+    with await condition:
+        print('worker3 start')
+        await asyncio.sleep(3)
+        print('worker3 end')
+        condition.notify_all()
+
+condition = asyncio.Event()
 loop.run_until_complete(asyncio.wait([
-    worker1(lock), worker2(lock)
+    worker1(condition), worker2(condition), worker3(condition)
 ]))
 loop.close()
