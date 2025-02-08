bc6507d683fc223692135511dd372bd73930fa8a
arata-takenouchi <arata.takenouchi.1230@gmail.com>
2025-02-08 22:06:26 +0900

    lesson36-9

--- lesson.py
+++ lesson.py
@@ -2,20 +2,19 @@ import asyncio
 
 loop = asyncio.get_event_loop()
 
-async def worker1(semaphore):
-    with await semaphore:
-        print('worker1 start')
-        await asyncio.sleep(3)
-        print('worker1 end')
+async def worker1(queue):
+    print('worker1 start')
+    await queue.put(100)
+    print('worker1 end')
 
-async def worker2(semaphore):
-    with await semaphore:
-        print('worker2 start')
-        await asyncio.sleep(3)
-        print('worker2 end')
+async def worker2(queue):
+    print('worker2 start')
+    x = await queue.get()
+    print(x)
+    print('worker2 end')
 
-semaphore = asyncio.Semaphore(2)
+queue = asyncio.Queue()
 loop.run_until_complete(asyncio.wait([
-    worker1(semaphore), worker2(semaphore)
+    worker1(queue), worker2(queue)
 ]))
 loop.close()
