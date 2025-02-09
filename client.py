74a1729c9c26f0b66264ddd4d03b45e8a50a65bc
arata-takenouchi <arata.takenouchi.1230@gmail.com>
2025-02-09 17:26:05 +0900

    lesson36-17

--- client.py
+++ client.py
@@ -30,12 +30,27 @@ class AsyncIterater(object):
             raise StopAsyncIteration
         return data
 
+class AsyncContextManager(object):
+    def __init__(self, name, loop):
+        self.enter = 'enter'
+        self.ac = AsyncIterater(name, loop)
+        self.exit = 'exit'
+
+    async def __aenter__(self):
+        print(self.enter)
+        await asyncio.sleep(3)
+        return self.ac
+
+    async def __aexit__(self, exc_type, exc_val, exc_tb):
+        print(self.exit)
+        await asyncio.sleep(3)
 
 async def main(name, loop):
     print('chunk reader')
     # result = await AwaitableClass(name, loop)
-    async for i in AsyncIterater(name, loop):
-        print(i)
+    async with AsyncContextManager(name, loop) as ac:
+        async for i in ac:
+            print(i)
 
 loop.run_until_complete(asyncio.wait([
     main('task1', loop), main('task2', loop)
