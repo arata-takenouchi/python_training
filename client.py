7311d838a970e1847aae87eaeffd83267dbf4089
arata-takenouchi <arata.takenouchi.1230@gmail.com>
2025-02-09 17:50:11 +0900

    lesson36-19

--- client.py
+++ client.py
@@ -1,4 +1,5 @@
 import asyncio
+import time
 
 loop =  asyncio.get_event_loop()
 
@@ -45,12 +46,26 @@ class AsyncContextManager(object):
         print(self.exit)
         await asyncio.sleep(3)
 
+import time
+import concurrent.futures
+def blocking_task(name):
+    print(f'block {name}')
+    time.sleep(1)
+    return f'done {name}'
+
 async def main(name, loop):
-    print('chunk reader')
+    # print('chunk reader')
     # result = await AwaitableClass(name, loop)
-    async with AsyncContextManager(name, loop) as ac:
-        async for i in ac:
-            print(i)
+    # async with AsyncContextManager(name, loop) as ac:
+    #     async for i in ac:
+    #         print(i)
+    def done_callback(future):
+        print(future.result())
+    executor = concurrent.futures.ProcessPoolExecutor(max_workers=5)
+    for i in range(4):
+        future = loop.run_in_executor(executor, blocking_task, name)
+        future.add_done_callback(done_callback)
+    executor.shutdown(wait=True)
 
 loop.run_until_complete(asyncio.wait([
     main('task1', loop), main('task2', loop)
