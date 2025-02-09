0ac4406192b3d567879ed47a3db6fe17c621a4bd
arata-takenouchi <arata.takenouchi.1230@gmail.com>
2025-02-09 14:42:53 +0900

    lesson36-10

--- lesson.py
+++ lesson.py
@@ -2,19 +2,20 @@ import asyncio
 
 loop = asyncio.get_event_loop()
 
-async def worker1(queue):
-    print('worker1 start')
-    await queue.put(100)
-    print('worker1 end')
+async def f(future):
+    await asyncio.sleep(1)
+    future.set_result('Future is done')
 
-async def worker2(queue):
-    print('worker2 start')
-    x = await queue.get()
-    print(x)
-    print('worker2 end')
+def got_result(future):
+    print(future.result())
+    loop.stop()
 
-queue = asyncio.Queue()
-loop.run_until_complete(asyncio.wait([
-    worker1(queue), worker2(queue)
-]))
+future = asyncio.Future()
+asyncio.ensure_future(f(future))
+
+future.add_done_callback(got_result)
+
+# loop.run_until_complete(future)
+# print(future.result())
+loop.run_forever()
 loop.close()
